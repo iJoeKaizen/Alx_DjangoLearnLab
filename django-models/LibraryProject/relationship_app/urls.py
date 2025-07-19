c219a1ab007149c9c21f543e2bcfa5a695b21c01
Gideon Mensah <mrgmens@gmail.com>
2025-07-19 14:20:00 +0000

    implementation of the provided templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import user_login, user_logout, user_register, list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView
 from django.contrib.auth.views import LoginView, LogoutView
 
 
