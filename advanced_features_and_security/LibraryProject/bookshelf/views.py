a953fd8df205573cf9ca6389c59eaf0c74b84ba5
Gideon Mensah <mrgmens@gmail.com>
2025-07-27 14:03:10 +0000

    Updated views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,23 +1,28 @@
-from django.contrib.auth.decorators import permission_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required, permission_required
+
 from .models import Book
+from .forms import BookForm, ExampleForm
 
-@permission_required('bookshelf.can_view', raise_exception=True)
-def book_list(request):
-    books = Book.objects.all()
-    return render(request, 'bookshelf/book_list.html', {'books': books})
-
+@login_required
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
-    # Your logic here
-    pass
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
 
-@permission_required('bookshelf.can_edit', raise_exception=True)
-def edit_book(request, pk):
-    # Your logic here
-    pass
-
-@permission_required('bookshelf.can_delete', raise_exception=True)
-def delete_book(request, pk):
-    # Your logic here
-    pass
+@login_required
+def example_form_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # handle form.cleaned_data here
+            return render(request, 'bookshelf/success.html', {'name': form.cleaned_data['name']})
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/example_form.html', {'form': form})
