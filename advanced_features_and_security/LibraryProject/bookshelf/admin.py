83bb1889405c59ba5062a74fa0aebad585eb826e
Gideon Mensah <mrgmens@gmail.com>
2025-07-27 13:26:10 +0000

    admin.site.register(CustomUser, CustomUserAdmin)

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import CustomUser, Book
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # Show these fields in the list
-    list_filter = ('author', 'publication_year')  # Add filters for easy browsing
-    search_fields = ('title', 'author')  # Add search box for title and author
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('author', 'publication_year')
+    search_fields = ('title', 'author')
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("email", "date_of_birth", "is_staff", "is_superuser")
+    list_filter = ("is_staff", "is_superuser", "is_active")
+    ordering = ("email",)
+    search_fields = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("date_of_birth", "profile_photo")}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "is_staff", "is_superuser"),
+        }),
+    )
