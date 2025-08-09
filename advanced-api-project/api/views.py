0ca88f2993a4ca6e3a0381de020fe720dfc3cffc
Gideon Mensah <mrgmens@gmail.com>
2025-08-09 18:21:42 +0000

    add advanced query capabilities to views that manage the Book model

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated  # required import
-
 from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer
 
@@ -55,3 +56,21 @@ class AuthorDeleteView(generics.DestroyAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
     permission_classes = [IsAuthenticated]
+
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    # Enable filtering, searching, ordering
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+
+    # Filtering
+    filterset_fields = ['title', 'author', 'publication_year']
+
+    # Searching
+    search_fields = ['title', 'author__name']
+
+    # Ordering
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']  # default ordering
